49efc736c4ff7e819735b14df66aa1b241ee805f
Mikel <mikelvallejo9@gmail.com>
2021-04-30 16:34:08 +0200

    entrega

--- vikingsClases.py
+++ vikingsClases.py
@@ -21,14 +21,13 @@ class Viking(Soldier):
         self.name = name
         self.health = health
         self.strength = strength
-    def attack(self):
-        return self.strength
-    def receiveDamage (self):
+
+    def receiveDamage (self, damage):
         self.health = self.health - damage
         if self.health > 0:
-            return(str(name) + ' has received ' + str(damage) + ' points of damage')
+            return(str(self.name) + ' has received ' + str(damage) + ' points of damage')
         else:
-            return (str(name) + ' has died in act of combat')
+            return (str(self.name) + ' has died in act of combat')
     def battleCry(self):
         return 'Odin Owns You All!'
 
@@ -40,12 +39,12 @@ class Saxon(Soldier):
     def attack(self):
         return self.strength
 
-    def receiveDamage(self):
+    def receiveDamage(self, damage):
         self.health = self.health - damage
         if self.health > 0:
-            return ('Saxon has received ' + str(damage) + ' points of damage')
+            return ('A Saxon has received ' + str(damage) + ' points of damage')
         else:
-            return ('Saxon has died in act of combat')
+            return ('A Saxon has died in combat')
 # War
 
 
