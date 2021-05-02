65c94c8aa7d1be8134c51fea6562de32c1b8536c
Mikel <mikelvallejo9@gmail.com>
2021-05-02 12:38:53 +0200

    update

--- vikingsClases.py
+++ vikingsClases.py
@@ -56,8 +56,24 @@ class War:
         self.vikingArmy.append(Viking)
     def addSaxon(self, Saxon):
         self.saxonArmy.append(Saxon)
-
     def vikingAttack(self):
-        return self.strength
+        receiveDamage(Saxon, self.strength)
+        if Saxon.health >0:
+            Saxon.saxonArmy.remove(Saxon)
+        return receiveDamage(Saxon, self.strength)
+    def saxonAttack(self):
+        receiveDamage(Viking, self.strength)
+        if Viking.health >0:
+            Viking.vikingArmy.remove(Viking)
+        return receiveDamage(Viking, self.strength)
+    def showStatus(self):
+        if len(vikingArmy(0)):
+            return 'Vikings have won the war of the century!'
+        elif len(saxoArmy(0)):
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
 
 
